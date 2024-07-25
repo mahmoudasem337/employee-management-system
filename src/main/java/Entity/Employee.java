a6ef2c20e0315116b51f85131b67112082aeb905
mahmoudasem <mahmoudasem337@gmail.com>
2024-07-26 01:59:14 +0300

    Create Employee.java
    Created Entity

---
+++ src/main/java/Entity/Employee.java
@@ -0,0 +1,70 @@
+package Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+private String firstname;
+    @Column(name = "last_name")
+private String lastname;
+    @Column(name = "email")
+private String email;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String firstname, String email, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.email = email;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
