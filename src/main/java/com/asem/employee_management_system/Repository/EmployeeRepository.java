e67119e4127f214252893e3d2e6643ba8aa7cb4c
mahmoudasem <mahmoudasem337@gmail.com>
2024-07-26 02:59:52 +0300

    Delete Repository

--- src/main/java/com/asem/employee_management_system/Repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.asem.employee_management_system.Repository;
-
-import Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,int> {
-}
